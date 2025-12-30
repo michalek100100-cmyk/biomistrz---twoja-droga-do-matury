020046b2f63030212b5cba053de19e0864ad7af5
Guests <guests@Daniels-MacBook-Air.local>
2025-12-30 15:29:32 +0100

    Naprawa intro, usunięcie expo i fix białego ekranu

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.biomistrz.app',
   appName: 'biomistrz---matura-z-biologii',
-  webDir: 'dist'
-};
+  webDir: 'dist',
 
+
+server: {
+  androidScheme: 'https'
+
+},
+plugins: {
+  CapacitorHttp: {
+    enabled: true,
+  },
+},
+};
 export default config;
